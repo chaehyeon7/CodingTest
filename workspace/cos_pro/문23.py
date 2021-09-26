c5a2e7f28e84b3e0026ae16a84ba141d8526a852
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-09-26 11:44:06 +0900

    코딩테스트 23문제

---
+++ workspace/cos_pro/문23.py
@@ -0,0 +1,14 @@
+def solution(scores):
+    return(sum(scores)-max(scores)-min(scores))
+    answer = 0
+    return answer
+
+scores1 = [35, 28, 98, 34, 20, 50, 85, 74, 71, 7]
+ret1 = solution(scores1)
+
+print("solution 함수의 반환 값은", ret1, "입니다.")
+
+scores2 = [1, 1, 1, 1, 1]
+ret2 = solution(scores2)
+
+print("solution 함수의 반환 값은", ret2, "입니다.")
