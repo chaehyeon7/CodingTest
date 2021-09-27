083f93f80cfa3fed6ea3595dc6e2c4d9956d53ac
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-09-27 20:52:40 +0900

    코딩테스트 24문제

---
+++ workspace/cos_pro/문24.py
@@ -0,0 +1,17 @@
+def solution(words, word):
+    count = 0
+    for comp in words:
+        for x,y in zip(comp,word):
+            if x != y:
+                count = count +1
+    return count
+
+
+
+
+
+words = ["CODE", "COED", "CDEO"]
+word = "CODE"
+ret = solution(words, word)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
