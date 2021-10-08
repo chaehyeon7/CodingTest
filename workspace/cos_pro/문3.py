8462e27c962298a8fbd9bdf6aef6625ed4109ab8
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-10-08 15:08:53 +0900

    코딩테스트 3번 문제 (8번 문제 수정)

--- workspace/cos_pro/문3.py
+++ workspace/cos_pro/문3.py
@@ -1,12 +1,18 @@
-def solution(characters):
-	result = ""
-	result += characters[0]
-	for i in range(1, len(characters)):
-		if characters[i - 1] != characters[i]:
-			result += characters[i]
-	return result
+def solution(arr, max_n, min_n):
+    cnt_arr = [0 for _ in range(1001)]
+    answer = 0
 
-characters = "senteeeencccccceeee"
-ret = solution(characters)
+    for i in arr:
+        cnt_arr[i] += 1
 
-print("solution 함수의 반환 값은", ret, "입니다.")
+    answer = cnt_arr[max_n] // cnt_arr[min_n]
+
+    return answer
+
+
+arr = [1, 2, 3, 3, 1, 3, 3, 2, 3, 2]
+max_num = max(arr)  # 염쌤은 리스트의 내장함수인 max와 min 함수를 사용했어요.
+min_num = min(arr)
+answer = solution(arr, max_num, min_num)
+
+print(max_num, "이", min_num, "보다 ", answer, "배 많이 들어있다.")
