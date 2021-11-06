8f60091d83007e4b3c56cd5bd0902c807ab54c0e
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2021-11-06 21:54:32 +0900

    코딩테스트 2차 문3번

---
+++ workspace/cos_pro/2차_문3.py
@@ -0,0 +1,12 @@
+def solution(N, M):
+    answer = 0
+    for i in range(N, M+1):
+        if i % 2==0:
+            answer += i**2
+        return answer
+
+N = 4
+M = 7
+ret = solution(N, M)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
